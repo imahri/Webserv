b49136a83631d4f388b7bbd1c2f7d89008d2d876
Imad-eddine Mahri <imahri@e3r4p5.1337.ma>
2023-11-24 22:33:21 +0100

    'added'

--- final_server.cpp
+++ final_server.cpp
@@ -1,4 +1,8 @@
+
+
+
 #include <cmath>
+#include <cstddef>
 #include <cstdio>
 #include <iostream>
 #include <cstring>
@@ -24,12 +28,9 @@ class Client
     public:
         int fd;
         bool isEnd;
-        std::string reque;
-        Client(int fd){
-            isEnd = false;
-            this->fd = fd;
-        };
-        ~Client(){};
+        char *buffer[1024];
+        Client(int fd):fd(fd),isEnd(false){};
+        ~Client();
 };
 
 class Server{
@@ -45,23 +46,6 @@ class Server{
         ~Server(){};
         static int nbr_srv;
         void    start();
-};
-
-class Webserve
-{
-    public:
-        std::vector<Server> sudo_apt;
-       
-        static std::vector<struct pollfd> net;
-        static int maxfd;
-        bool    isServer(int fd);
-        void    acceptNewClient(int fd);
-        int     foundServer(int fd);
-        int     checkClient(int fd);
-        int     checkServer(int fd);
-        bool    isDoneRequest(char *str, int fd, int index);
-        Webserve(){};
-        ~Webserve(){};
 };
 
 int Server::nbr_srv = 0;
@@ -90,8 +74,6 @@ Server::Server(int port, std::string    ip)
 //     listen(serversocket, 1000);
 //     nbr_srv++;
 // }
-
-
 void Server::start()
 {
     this->serversocket = socket(AF_INET, SOCK_STREAM, 0);
@@ -111,30 +93,32 @@ void Server::start()
     nbr_srv++;
 }
 
+class Webserve
+{
+    public:
+        std::vector<Server> sudo_apt;
+       
+        static std::vector<struct pollfd> net;
+        static int maxfd;
+        bool    isServer(int fd);
+        void    acceptNewClient(int fd);
+        int     foundServer(int fd);
+        std::vector<Client *>::iterator         checkClient(int fd);
+        std::vector<Server>::iterator           checkServer(int fd);
+        bool    isDoneRequest(char *str);
+        Webserve(){};
+        ~Webserve(){};
+};
 
 int Webserve::maxfd = 0;
 std::vector<struct pollfd> Webserve::net;
 
 
-bool    Webserve::isDoneRequest(char *str, int fd, int index)
+bool    Webserve::isDoneRequest(char *str)
 {
-    // std::cout << fd << "\t" << index << std::endl;
-    // std::cout << Webserve::sudo_apt[fd].sudo_client[index]->isEnd << std::endl;
-    // if(Webserve::sudo_apt[index].sudo_client[fd]->isEnd == false)
-    // {
-        // Webserve::sudo_apt[index].sudo_client[fd]->reque += str;
-        // size_t     first = Webserve::sudo_apt[index].sudo_client[fd]->reque.find_first_of("\r\n\r\n");
-        // if(first != Webserve::sudo_apt[index].sudo_client[fd]->reque.npos)
-        // {
-        //     size_t     last = Webserve::sudo_apt[index].sudo_client[fd]->reque.find_last_of("\r\n\r\n");
-        //     if(last != Webserve::sudo_apt[index].sudo_client[fd]->reque.npos && last != first)
-        //     {
-        //         Webserve::sudo_apt[index].sudo_client[fd]->isEnd = true;
-        //         return true;
-        //     }
-        // }
-    // }
-    return(true);
+    if (strcmp("/r/n/r/n", str))
+        return true;
+    return false;
 }
 
 bool isElementInVector(std::vector<int>& vec, int element)
@@ -173,51 +157,56 @@ int     Webserve::foundServer(int fd)
 void    Webserve::acceptNewClient(int fd)
 {
     int clientSocket = accept(fd, NULL, 0);
-    // bzero(void *, size_t);
     struct pollfd tmp;
     tmp.fd = clientSocket;
-    tmp.events = POLLIN;
+    tmp.events = POLLIN | POLLOUT;
     fcntl(clientSocket, F_SETFL, O_NONBLOCK, FD_CLOEXEC);
     Webserve::net.push_back(tmp);
     int finder = foundServer(fd);
-    Client *cr = new Client(clientSocket);
-    sudo_apt[finder].sudo_client.push_back(cr);
-
+    if (finder != -1)
+    {
+        Client *cr = new Client(clientSocket);
+        sudo_apt[finder].sudo_client.push_back(cr);
+    }
 }
 
-int    Webserve::checkClient(int fd)
+std::vector<Client *>::iterator    Webserve::checkClient(int fd)
 {
+    std::vector<Client *>::iterator fin;
     std::vector<Server>::iterator itr = sudo_apt.begin();
-    for(size_t i = 0; i < sudo_apt.size(); i++)
+    for(; itr != sudo_apt.end(); itr++)
     {
-        for(size_t j = 0; sudo_apt[i].sudo_client.size(); j++)
+        std::vector<Client *>::iterator itrs = itr->sudo_client.begin();
+        for(; itrs != itr->sudo_client.end(); itrs++)
         {
-            if (fd == sudo_apt[i].sudo_client[j]->fd)
-                return j;
+            if (fd == (*itrs)->fd)
+                fin =  itrs;
         }
     }
-    return 0;
+    return fin;
 }
 
-int    Webserve::checkServer(int fd)
+std::vector<Server>::iterator    Webserve::checkServer(int fd)
 {
+    std::vector<Server>::iterator fin;
     std::vector<Server>::iterator itr = sudo_apt.begin();
-    for(size_t i = 0; i < sudo_apt.size(); i++)
+    for(; itr != sudo_apt.end(); itr++)
     {
-        for(size_t j = 0; sudo_apt[i].sudo_client.size(); j++)
+        std::vector<Client *>::iterator itrs = itr->sudo_client.begin();
+        for(; itrs != itr->sudo_client.end(); itrs++)
         {
-            if (fd == sudo_apt[i].sudo_client[j]->fd)
-                return i;
+            if (fd == (*itrs)->fd)
+                fin =  itr;
         }
     }
-    return 0;
+    return fin;
 }
 
 int main()
 {
     Webserve re;
-    Server qw(3005,"10.13.4.2");
-    Server qwe(3006,"10.13.4.2");
+    Server qw(3005,"0.0.0.0");
+    Server qwe(3006,"0.0.0.0");
     char buffer[3001];
     std::vector<Server> tr;
     tr.push_back(qw);
@@ -253,6 +242,7 @@ int main()
                         {
                             bzero(buffer, 3000);
                             int tt = recv(Webserve::net[j].fd, buffer,3000, 0);
+                            std::cout << "===>> " << tt << std::endl;
                             if (tt == 0)
                             {
                                 close(Webserve::net[j].fd);
@@ -261,29 +251,23 @@ int main()
                                 break;
                             std::cout << buffer << std::endl;
                         }
-                        int inclient = re.checkClient(Webserve::net[j].fd);
-                        int inserver = re.checkServer(Webserve::net[j].fd);
-                        // should be called
-
-
-                        std::cout <<"inserver: " << inserver << "\tinclient: " << inclient << std::endl;
-                        std::cout << "stttt: " << re.sudo_apt[inserver].sudo_client[inclient]->reque << std::endl;
-                        // std::cout << "msg: " << re.sudo_apt[0].sudo_client[j]->reque << std::endl;
-                        // std::cout << "fd: " << re.sudo_apt[0].sudo_client[j]->fd << std::endl;
-                        
-                        Webserve::net[j].events = POLLOUT;
-                        Webserve::net[j].revents = 0;
+                        if (re.isDoneRequest(buffer))
+                        {
+                            // int sta = re.checkClient(Webserve::net[j].fd);
+                            std::cout << "XXXX" << std::endl;
+                            Webserve::net[j].events = POLLOUT;
+                            Webserve::net[j].revents = 0;
+                        }
                         continue;
                     }
                     else if (Webserve::net[j].revents & POLLOUT){
                         // std::cout << Webserve::net[j].fd << std::endl;
                         //response
                         // if data end
-                        const char response[50000] = "HTTP/1.1 200 OK\r\nContent-Length: 10\r\nContent-Type: text/plain\r\n\r\n"; // here start the response send 500000
-                        const char message[50000] = "hacker men";
+                        const char *response = "HTTP/1.1 200 OK\r\nContent-Length: 10\r\nContent-Type: text/plain\r\n\r\n"; // here start the response send 500000
+                        const char *message = "hacker men";
                         send(Webserve::net[j].fd, response, strlen(response), 0);
                         send(Webserve::net[j].fd, message, strlen(message), 0);
-                        // if (re.isDoneRequest(buffer))
                         Webserve::net[j].events = POLLIN;
 
                     }
